dc91bffe5d62a520a23e390b3912f0c1721904e2
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2023-02-03 12:44:52 +0530

    updated env

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
-
+require ('dotenv').config();
 
 //mongodb+srv://sanjeev:<Tf9o9y6qowCCNGdd>@cluster0.0dpacjq.mongodb.net/test
+
 
 
 
 // mongoose.connect('mongodb://localhost:27017/firstnodejsproject',
 //nodejs 18 update 
 // mongoose.connect('mongodb://127.0.0.1:27017/firstnodejsproject',
-mongoose.connect('mongodb+srv://sanjeev:Tf9o9y6qowCCNGdd@cluster0.0dpacjq.mongodb.net/test',
+mongoose.connect(process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     // useFindAndModify: false,
