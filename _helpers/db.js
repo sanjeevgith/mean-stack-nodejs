9e4a7a284120db8bbe972b701abfe3358ee2198c
sanjeevgith <prajapatisanjiv8@gmail.com.com>
2023-02-03 12:32:58 +0530

    updated

--- _helpers/db.js
+++ _helpers/db.js
@@ -9,7 +9,7 @@ const mongoose = require("mongoose");
 // mongoose.connect('mongodb://localhost:27017/firstnodejsproject',
 //nodejs 18 update 
 // mongoose.connect('mongodb://127.0.0.1:27017/firstnodejsproject',
-mongoose.connect('mongodb+srv://sanjeev:<Tf9o9y6qowCCNGdd>@cluster0.0dpacjq.mongodb.net/test',
+mongoose.connect('mongodb+srv://sanjeev:Tf9o9y6qowCCNGdd@cluster0.0dpacjq.mongodb.net/test',
   {
     useNewUrlParser: true,
     // useFindAndModify: false,
